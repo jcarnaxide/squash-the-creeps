1f3d1d6a03f9ce6708a5477a6ab2053c442e65ff
Jesse Carnaxide <jcarnaxide@gmail.com>
2023-04-22 01:13:35 -0400

    started animation, need to setup for mobs

--- src/Player.gd
+++ src/Player.gd
@@ -34,6 +34,10 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
 
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
+
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -63,6 +67,8 @@ func _physics_process(delta):
 				# If so, we squash it and bounce.
 				mob.squash()
 				target_velocity.y = bounce_impulse
+
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 	# Moving the Character
 	velocity = target_velocity
