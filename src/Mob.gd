e6e2aeeec53709f5ddd162018d5c2f3c2d15fc43
Jesse Carnaxide <jcarnaxide@gmail.com>
2023-04-22 19:25:04 -0400

    completed the project

--- src/Mob.gd
+++ src/Mob.gd
@@ -28,6 +28,8 @@ func initialize(start_position, player_position):
 	# We then rotate the velocity vector based on the mob's Y rotation
 	# in order to move in the direction the mob is looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	
+	$AnimationPlayer.speed_scale = random_speed / min_speed
 
 
 func squash():
