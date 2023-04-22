b78770d5e82076f565cd2a43bfd55e21f338a6d1
Jesse Carnaxide <jcarnaxide@gmail.com>
2023-04-22 00:37:54 -0400

    keep track of score, add ui components, add music

--- src/Main.gd
+++ src/Main.gd
@@ -2,10 +2,15 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UserInterface/Retry.hide()
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
+	
+	# We connect the mob to the score label to update the score upon squashing one.
+	mob.squashed.connect($UserInterface/ScoreLabel._on_Mob_squashed.bind())
 
 	# Choose a random location on the SpawnPath.
 	# We store the reference to the SpawnLocation node.
@@ -22,3 +27,9 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
